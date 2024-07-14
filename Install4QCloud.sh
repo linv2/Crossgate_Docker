f6fb0a2c0c017dfd3e567a06a60b2e185d9289b7
Linv2 <libing274717158@163.com>
2024-07-15 11:40:54 +1200

    update shell from qcloud

--- Install4QCloud.sh
+++ Install4QCloud.sh
@@ -22,14 +22,16 @@ SCRIPT_DIR="$(dirname "$REAL_SCRIPT_PATH")"
 if command -v docker > /dev/null; then
     echo "docker已安装"
 else
-    echo "docker命令未找到，正在尝试安装..."  
-    sudo apt-get update  # 更新包列表  
-    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common  # 安装依赖包  
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -  # 添加Docker官方GPG密钥  
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"  # 添加Docker官方源  
-    sudo apt-get update  # 再次更新包列表以确保Docker源生效  
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io  # 安装Docker CE  
-    sudo systemctl start docker  # 启动Docker服务  
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://mirrors.cloud.tencent.com/docker-ce/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://mirrors.cloud.tencent.com/docker-ce/linux/ubuntu/ \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    systemctl status docker
 fi
 
 # 检查docker命令是否存在，如果不存在则安装Docker 
